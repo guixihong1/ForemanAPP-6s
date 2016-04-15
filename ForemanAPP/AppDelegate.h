bf9140913145b8500525dbd6bc2d65b1a4479c64
桂 <mac@bogon>
2016-04-15 17:38:36 +0800

    Initial Commit

---
+++ ForemanAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ForemanAPP
+//
+//  Created by mac on 16/4/15.
+//  Copyright © 2016年 YF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ForemanAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ForemanAPP
+//
+//  Created by mac on 16/4/15.
+//  Copyright © 2016年 YF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
